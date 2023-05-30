a6a7907fd780769b895d9f0f9bca93003db099bb
ImTaliesin <brennanjdouglas@gmail.com>
2023-05-30 16:46:45 -0700

    stuff

--- LengthChecker.js
+++ LengthChecker.js
@@ -1,2 +1,2 @@
-// var tweet=prompt("Enter your tweet here:")
-// alert("You have written "+tweet.length+" characters, you have "+(140-tweet.length)+" characters remaining.");
+var tweet=prompt("Enter your tweet here:")
+alert("You have written "+tweet.length+" characters, you have "+(140-tweet.length)+" characters remaining.");
