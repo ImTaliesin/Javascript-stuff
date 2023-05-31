e1caf37b59a8d3995379abd814e9c88ea6473b0d
ImTaliesin <brennanjdouglas@gmail.com>
2023-05-31 16:34:51 -0700

    Update Leapyeartest.js

--- Leapyeartest.js
+++ Leapyeartest.js
@@ -1,6 +1,5 @@
 function isLeap(year) {
-    if (year % 4 === 0) {
-        if (year % 100 === 0) {
+    if (year % 4 === 0) {if (year % 100 === 0) {
             if(year % 400 === 0) {
                 return "Leap year.";} 
     else {
