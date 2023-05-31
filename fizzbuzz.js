9a6b7faae5d0362e8b78db9335003679e5304dd3
ImTaliesin <brennanjdouglas@gmail.com>
2023-05-31 16:37:07 -0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,7 @@
 var output = [];
 var count = 1;
 
-while (count < 101) {
+while (count < 100) {
   if (count % 3 === 0 && count % 5 === 0) {
     output.push("FizzBuzz");
   } else if (count % 3 === 0) {
