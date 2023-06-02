e7d7015180c6d66559adb47d0853abda0db77d70
ImTaliesin <brennanjdouglas@gmail.com>
2023-06-01 18:36:22 -0700

    Created 99bottlesofbeer.js

---
+++ 99bottlesofbeer.js
@@ -0,0 +1,5 @@
+var num = 99;
+while (num >0){
+console.log(num + " bottles of beer on the wall, " + num + " bottles of beer.\n" 
++ "Take one down and pass it around, " + (num - 1) + " bottles of beer on the wall.\n\n");
+num = num -1;}
