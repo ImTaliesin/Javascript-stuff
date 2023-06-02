db80b1ba604e38842ed76d52d9aa6d03a6a941cf
ImTaliesin <brennanjdouglas@gmail.com>
2023-06-01 18:54:47 -0700

    Updated 99bottles to use a for loop to be cleaner

--- 99bottlesofbeer.js
+++ 99bottlesofbeer.js
@@ -1,6 +1,4 @@
-var num = 99;
-while (num >0){
+for (var num = 1000000; num > 0; num--) {
 console.log(num + " bottles of beer on the wall, " + num + " bottles of beer.\n" 
-+ "Take one down and pass it around, " + (num - 1) + " bottles of beer on the wall.\n\n");
-num = num -1;}
++ "Take one down and pass it around, " + (num - 1) + " bottles of beer on the wall.\n\n");}
 console.log("No more bottles of beer on the wall, no more bottles of beer.\n");
