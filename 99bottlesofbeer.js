f7b0deafc15a1b04ed0bcbc9aeb7256d1bfff631
ImTaliesin <brennanjdouglas@gmail.com>
2023-06-01 18:37:11 -0700

    Update 99bottlesofbeer.js

--- 99bottlesofbeer.js
+++ 99bottlesofbeer.js
@@ -3,3 +3,4 @@ while (num >0){
 console.log(num + " bottles of beer on the wall, " + num + " bottles of beer.\n" 
 + "Take one down and pass it around, " + (num - 1) + " bottles of beer on the wall.\n\n");
 num = num -1;}
+console.log("No more bottles of beer on the wall, no more bottles of beer.\n");
