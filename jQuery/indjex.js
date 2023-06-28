70c4f8f907e61825a5fa2d0c44a9c42c2574afa7
ImTaliesin <brennanjdouglas@gmail.com>
2023-06-27 22:02:44 -0700

    Learning more jQuery

--- jQuery/indjex.js
+++ jQuery/indjex.js
@@ -1,6 +1,21 @@
 
-$(document).ready(function(){
-    
-$("h1").css("color", "red")
 
+
+$("h1").addClass("big-title margin-50");
+
+$("a").attr("href", "https://www.yahoo.com");
+
+$("button").click(function() {
+    $("h1").css("color", "blue");
+    });
+
+$(document).keypress(function(event) {
+    var table = []
+    table.push(event.key);
+    $("h1").text(table);
 });
+
+$("button").on("click", function() {
+    $("h1").slideUp().slideDown().animate({opacity: Math.random()});
+}
+);
