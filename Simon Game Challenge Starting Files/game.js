be734425aa80d7080c3060b610f163262e355076
ImTaliesin <brennanjdouglas@gmail.com>
2023-06-29 16:49:42 -0700

    I broke the game and im stuck

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,21 +1,37 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userPattern = [];
+let audio = new Audio("sounds/" + name + ".mp3");
 
+gameStart();
+function gameStart() {
+    $(document).keypress(function() {
+    
+        nextSequence();
+    });
+}
 
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColor = buttonColors[randomNumber];
+ function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    $("h1").text("Level " + gamePattern.length);
+    $("." + randomChosenColor).fadeOut(100).fadeIn(100); //flashes the button
+    
+    //wait loop for 500ms
+setTimeout(() => {
+    playSound(randomChosenColor);
+    animatePress(randomChosenColor);
+}, 1000);
+
     console.log(gamePattern);
 }
 
+
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3");
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
-
-
 
 function animatePress(currentColor) {
     $("." + currentColor).addClass("pressed");
@@ -25,11 +41,28 @@ function animatePress(currentColor) {
     , 100);
 }
 
-
 $(".btn").click(function() {
-    var userChosenColor = $(this).attr("id");
-    userPattern.push(userChosenColor);
-    playSound(userChosenColor);
+    let userChosenColor = $(this).attr("id"); 
+    userPattern.push(userChosenColor);        
+    playSound(userChosenColor);              
     animatePress(userChosenColor);
+    console.log(userPattern);
+    checkAnswer();
+
+
+
+    
+});
+
+function checkAnswer() {
+    if (userPattern == gamePattern) {
+        console.log("success");
+        nextSequence();
+
+    }
+    else {
+        console.log("wrong");
+
+
+    }
 }
-);
