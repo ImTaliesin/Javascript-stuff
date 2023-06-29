8f945d4c7233482b6e6de1e5562affb29dd0f0b9
ImTaliesin <brennanjdouglas@gmail.com>
2023-06-28 20:48:33 -0700

    added jquery animation, playing sound, buttons

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,35 @@
+let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userPattern = [];
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    console.log(gamePattern);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+
+
+function animatePress(currentColor) {
+    $("." + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("." + currentColor).removeClass("pressed");
+    }
+    , 100);
+}
+
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+    userPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+}
+);
